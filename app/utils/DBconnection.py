1cde330d475c367fa900ab53d0e61a1698f19efb
BaiAsuka <296010236@qq.com>
2019-04-24 22:19:54 +0800

    新增数据库工具

--- app/utils/DBconnection.py
+++ app/utils/DBconnection.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 from ..settings import config
+from contextlib import contextmanager
 
 class DB_Connection:
     def __init__(self, host=None, port=None, username=None, password=None, db_name=None):
@@ -9,7 +12,57 @@ class DB_Connection:
         self.password = password if password else config.DB_PASSWORD
         self.dbs = db_name if db_name else config.DB_NAME
 
+        self.engine = self.get_engine()
+
     def get_engine(self):
         print(self.dbs)
         return create_engine('mysql+mysqldb://{username}:{password}@{host}:{port}/{dbs}'.format(
             username=self.username, password=self.password, host=self.host, port=self.port, dbs=self.dbs))
+
+    def get_session(self):
+        Session = sessionmaker(bind=self.engine)
+        return Session
+
+Session = DB_Connection().get_session()
+
+@contextmanager
+def open_session():
+    """
+    可以使用 with 上下文，在 with 结束之后自动 commit
+    """
+    _session = Session()
+    try:
+        yield _session
+        _session.commit()
+    except Exception as e:
+        # _session.rollback()
+        raise e
+    finally:
+        _session.close()
+
+def excute_query(sql, params):
+    with open_session() as session:
+        try:
+            result = session.excute(text(sql), params).fetchall()
+        except Exception as e:
+            session.rollback()
+        else:
+            return result
+
+def excute_update(sql, params):
+    with open_session() as session:
+        try:
+            session.excute(text(sql), params)
+        except Exception as e:
+            session.rollback()
+        else:
+            return True
+
+def excute_delete(sql, params):
+    with open_session() as session:
+        try:
+            session.excute(text(sql), params)
+        except Exception as e:
+            session.rollback()
+        else:
+            return True
